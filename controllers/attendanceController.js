a101f3d41908e9272fd36b117ef0d2af02a0a039
RahulNC0629 <rahul.nc@avaali.com>
2026-05-24 00:25:20 +0530

    fixed the deployment error

--- controllers/attendanceController.js
+++ controllers/attendanceController.js
@@ -121,7 +121,61 @@ export const clockIn = async (req, res) => {
 
 
 // Clock Out
+export const clockOut = async (req, res) => {
+    try {
+        const { employeeId } = req.body;
+        const today = new Date().toISOString().split("T")[0];
+        const attendance = await Attendance.findOne({ employeeId, date: today, });
+        if (!attendance) {
+            return res.status(404).json({ message: "No clock in found" })
+        }
+        if (attendance.checkOutTime) {
+            return res.status(400).json({ message: "Already clocked out" })
+        }
+        const logoutDateTime = new Date();
+        attendance.checkOutTime = logoutDateTime.toLocaleTimeString("en-US", { hour: "2-digit", minute: "2-digit", second: "2-digit", hour12: true, });
+        // Calculate Hours 
+        const diffMs = logoutDateTime - attendance.loginTime;
+        const totalMinutes = Math.floor(diffMs / 1000 / 60);
+        // Gross Hours
+        const grossHours = Math.floor(totalMinutes / 60);
+        const grossMinutes = totalMinutes % 60;
+        attendance.grossHours = `${grossHours} h ${grossMinutes} m`;
+        // Effective Hours 
+        // const effectiveTotalMinutes = Math.max(totalMinutes - attendance.breakMinutes, 0); 
+        // const effectiveHours = Math.floor(effectiveTotalMinutes / 60); 
+        // const effectiveMinutes = effectiveTotalMinutes % 60; 
+        // attendance.effectiveHours = ${effectiveHours}h ${effectiveMinutes}m; 
+        attendance.status = "Present"; await attendance.save();
+        res.status(200).json({ message: "Clock Out Successful", attendance, });
+    } catch (error) {
+        res.status(500).json({ message: error.message, })
+    }
+};
 
+
+
+// Get User Attendance
+
+export const getMyAttendance = async (req, res) => {
+    try {
+        const { employeeId } = req.params;
+        const attendance = await Attendance.find({
+            employeeId
+        }).sort({
+            createdAt: -1,
+        });
+
+        res.status(200).json(attendance);
+
+    } catch (error) {
+        res.status(500).json({
+            message: error.message,
+        })
+    }
+}
+
+// Request Regularization
 export const requestRegularization = async (req, res) => {
     try {
         const {
@@ -192,90 +246,6 @@ export const requestRegularization = async (req, res) => {
         });
     }
 };
-
-// Get User Attendance
-
-export const getMyAttendance = async (req, res) => {
-    try {
-        const { employeeId } = req.params;
-        const attendance = await Attendance.find({
-            employeeId
-        }).sort({
-            createdAt: -1,
-        });
-
-        res.status(200).json(attendance);
-
-    } catch (error) {
-        res.status(500).json({
-            message: error.message,
-        })
-    }
-}
-
-// Request Regularization
-export const requestRegularization = async (req, res) => {
-    try {
-        const {
-            attendanceId,
-            regularizationReason,
-            regularizedCheckIn,
-            regularizedCheckOut,
-        } = req.body;
-
-        const attendance = await Attendance.findById(attendanceId);
-
-        if (!attendance) {
-            return res.status(404).json({
-                message: "Attendance not found",
-            });
-        }
-
-        // 24 Hours Validation
-        const attendanceDate = new Date(attendance.date);
-        const currentDate = new Date();
-        attendanceDate.setHours(23, 59, 59, 999);
-
-        if (currentDate > attendanceDate) {
-            return res.status(400).json({
-                message: "Regularization allowed only on same day",
-            });
-        }
-
-        attendance.regularizationStatus = "Pending";
-        attendance.regularizationReason = regularizationReason;
-        attendance.regularizedCheckIn = regularizedCheckIn;
-        attendance.regularizedCheckOut = regularizedCheckOut;
-
-        if (
-            !regularizedCheckIn ||
-            !regularizedCheckOut ||
-            !regularizationReason
-        ) {
-            return res.status(400).json({
-                message:
-                    "Check In, Check Out and Reason are required",
-            });
-        }
-
-        if (attendance.regularizationStatus === "Pending") {
-            return res.status(400).json({
-                message:
-                    "Regularization already requested",
-            });
-        }
-
-        await attendance.save();
-
-        res.status(200).json({
-            message: "Regularization request submitted successfully",
-        });
-    } catch (error) {
-        res.status(500).json({
-            message: error.message,
-        })
-    }
-}
 
 // Get All Regularizations
 export const getRegularizationRequests = async (req, res) => {
